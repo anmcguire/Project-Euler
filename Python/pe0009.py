34140bfbf2f33f59f587d7b9353ad8b8e6948ac5
Andrew McGuore <anmcguire@gmail.com>
2021-07-25 00:36:43 -0500

    Adding pe0008.py, pe0009.py, and pe0010.py.

---
+++ Python/pe0009.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+for a in range(1, 1001):
+    for b in range(a, 1001):
+        c = 1000 - (a + b)
+        if c <= b:
+            pass
+        else:
+            if a ** 2 + b ** 2 == c ** 2:
+                print(a * b * c)
---
+++ Python/pe0010.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import math
+
+def is_prime(n):
+    if n < 2:
+        return False
+    else:
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if n % i == 0:
+                return(False)
+    return(True)
+
+
+print(sum(filter(lambda x: is_prime(x), range(1, int(2E6) + 1))))
