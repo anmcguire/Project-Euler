e73d549713b8242594d7cfc9cb73e0ad3a09bffa
Andrew McGuore <anmcguire@gmail.com>
2021-07-31 13:37:15 -0500

    Simplified code, changed set to list.

--- Python/pe0003.py
+++ Python/pe0003.py
@@ -3,11 +3,10 @@
 import math
 
 def factors(n):
-    factors = set()
+    factors = []
     for i in range(1, int(math.sqrt(n)) + 1):
         if n % i == 0:
-            factors.add(i)
-            factors.add(n / i)
-    return factors
+            factors += [i, n / i]
+    return(factors)
 
 print(max(filter(lambda x: len(factors(x)) == 2, [x for x in factors(600851475143)])))
